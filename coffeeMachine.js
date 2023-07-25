fb843f37ff998e574ce953929aca0d6977bb8e05
Kenneth Benitez <111894474+InfiniteExalt@users.noreply.github.com>
2023-07-25 14:19:57 -0500

    Update coffeeMachine.js

--- coffeeMachine.js
+++ coffeeMachine.js
@@ -1,69 +1,134 @@
-const input = require('sync-input');
+// Use "input()" to input a line from the user
+// Use "input(str)" to print some text before requesting input
+// You will need this in the following stages
+const input = require('sync-input')
 
-let cm = {'water': 400, 'milk': 540, 'beans': 120, 'cups': 9, 'sum': 550};
-let kinds = [{'water': 250, 'milk': 0, 'beans': 16, 'cost': 4},
-    {'water': 350, 'milk': 75, 'beans': 20, 'cost': 7},
-    {'water': 200, 'milk': 100, 'beans': 12, 'cost': 6}];
+const CoffeeMachine = {
+    isRunning: true,
+    moneyAvailable: 550,
+    disposableCups: 9,
+    qtWater: 400,
+    qtMilk: 540,
+    qtCoffeeBeans: 120,
+    coffeeTypes: [
+        {
+            name: "Espresso",
+            requiredWater: 250,
+            requiredCoffeeBeans: 16,
+            requiredMilk: 0,
+            price: 4
+        },
+        {
+            name: "Latte",
+            requiredWater: 350,
+            requiredCoffeeBeans: 20,
+            requiredMilk: 75,
+            price: 7
+        },
+        {
+            name: "Cappuccino",
+            requiredWater: 200,
+            requiredCoffeeBeans: 12,
+            requiredMilk: 100,
+            price: 6
+        }
+    ],
+    operations: [
+        function buyCoffee() {
+            const coffeeType = input("What do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino, back - to main menu: ");
 
-function state() {
-    console.log(`\nThe coffee machine has:
-${cm.water} ml of water
-${cm.milk} ml of milk
-${cm.beans} g of coffee beans
-${cm.cups} disposable cups
-$${cm.sum} of money`);
-}
+            switch (coffeeType) {
+                case '1':
+                    this.makeCoffee('Espresso');
+                    break;
+                case '2':
+                    this.makeCoffee('Latte');
+                    break;
+                case '3':
+                    this.makeCoffee('Cappuccino');
+                    break;
+                case 'back':
+                    this.init();
+                    break;
+            }
+        },
+        function fillCoffee() {
+            const waterToAdd = input("Write how many ml of water you want to add: ");
+            const milkToAdd = input("Write how many ml of milk you want to add: ");
+            const beansToAdd = input("Write how many grams of coffee beans you want to add: ");
+            const cupsToAdd = input("Write how many disposable cups you want to add: ");
 
-function buy() {
-    let kind = input("\nWhat do you want to buy? 1 - espresso, 2 - latte, 3 - cappuccino:\n");
-    if (kind != "back") {
-        kind -= 1;
-        if (cm.water < kinds[kind].water) {
-            console.log("Sorry, not enough water!");
-        } else if (cm.milk < kinds[kind].milk) {
-            console.log("Sorry, not enough milk!");
-        } else if (cm.beans < kinds[kind].beans) {
-            console.log("Sorry, not enough coffee beans!");
-        } else if (cm.cups < kinds[kind].cups) {
-            console.log("Sorry, not enough cups!");
-        } else {
-            cm.water -= kinds[kind].water;
-            cm.milk -= kinds[kind].milk;
-            cm.beans -= kinds[kind].beans;
-            cm.sum += kinds[kind].cost;
-            cm.cups -= 1;
+            this.qtMilk += parseInt(milkToAdd);
+            this.qtCoffeeBeans += parseInt(beansToAdd);
+            this.qtWater += parseInt(waterToAdd);
+            this.disposableCups += parseInt(cupsToAdd);
+        },
+        function takeMoney() {
+            console.log(`I gave you ${this.moneyAvailable}`);
+            this.moneyAvailable = 0;
+        }
+    ],
+    setOperation() {
+        const operation = input("Write action (buy, fill, take, remaining, exit): ").toLowerCase();
+        const [ buyCoffee, fillCoffee, takeMoney ] = this.operations;
+
+        switch (operation) {
+            case 'buy':
+                buyCoffee.call(this);
+                break;
+            case 'fill':
+                fillCoffee.call(this);
+                break;
+            case 'take':
+                takeMoney.call(this);
+                break;
+            case 'remaining':
+                this.showState();
+                break;
+            case 'exit':
+                this.isRunning = false;
+        }
+    },
+    makeCoffee(coffeeType) {
+        const coffee = this.coffeeTypes.find(coffee => coffee.name === coffeeType);
+
+        if (this.qtWater < coffee.requiredWater) {
+            console.log("Sorry, not enough water!\n");
+            return;
+        } else if (this.qtMilk < coffee.requiredMilk) {
+            console.log("Sorry, not enough milk!\n");
+            return;
+        } else if (this.qtCoffeeBeans < coffee.requiredCoffeeBeans) {
+            console.log("Sorry, not enough coffee beans!\n");
+            return;
+        } else if (this.disposableCups < 1) {
+            console.log("Sorry, not enough disposable cups!\n");
+            return;
+        }
+
+        console.log("I have enough resources, making you a coffee!\n");
+
+        if (coffee) {
+            this.moneyAvailable += coffee.price;
+            this.disposableCups -= 1;
+            this.qtCoffeeBeans -= coffee.requiredCoffeeBeans;
+            this.qtMilk -= coffee.requiredMilk;
+            this.qtWater -= coffee.requiredWater;
+        }
+    },
+    showState() {
+        console.log("The coffee machine has:");
+        console.log(`${this.qtWater} ml of water`);
+        console.log(`${this.qtMilk} ml of milk`);
+        console.log(`${this.qtCoffeeBeans} g of coffee beans`);
+        console.log(`${this.disposableCups} disposable cups`);
+        console.log(`$${this.moneyAvailable} of money\n`);
+    },
+    init() {
+        while (this.isRunning) {
+            this.setOperation();
         }
     }
 }
 
-function fill() {
-    cm.water += parseInt(input("Write how many ml of water you want to add:\n"));
-    cm.milk += parseInt(input("Write how many ml of milk you want to add:\n"));
-    cm.beans += parseInt(input("Write how many grams of coffee beans you want to add:\n"));
-    cm.cups += parseInt(input("Write how many disposable coffee cups you want to add:\n"));
-}
-
-function take() {
-    console.log(`I gave you ${cm.sum}\n`);
-    cm.sum = 0;
-}
-
-while(true) {
-    let cmd = input(`\nWrite action (buy, fill, take, remaining):\n`);
-    if (cmd === 'buy') {
-        buy();
-    }
-    if (cmd === 'fill') {
-        fill();
-    }
-    if (cmd === 'take') {
-        take();
-    }
-    if (cmd === "remaining") {
-        state();
-    }
-    if (cmd === "exit") {
-        break;
-    }
-}
-
+CoffeeMachine.init();
